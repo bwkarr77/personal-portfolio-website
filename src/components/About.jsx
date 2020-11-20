46fc96630028b7deb6ce35d2b18e7135c4bedffe
bwkarr77 <b.karr@polyclip.us>
2020-11-19 21:42:20 -0600

    updated the about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,26 +2,46 @@ import React from "react";
 
 const About = () => {
   return (
-    <article id="about">
-      <h2 className="major">About</h2>
-      <span className="image main">
-        <img src="images/Dancing.jpg" alt="" className='img-main'/>
-      </span>
-      <p>
-        My name is Brett. I come from a small town of Carpinteria, CA. Growing
-        up I played in the marching band, some sports, and a few random
-        recreational activities. Eagle Scout, power lifter, weight loss(er), Smash Brothers enthusiast, and snacker are adjectives that I've gone by in the past.
-      </p>
-      <p>
-        After high school I spent far too long at a local city college (SBCC),
-        where I dabbled in most scientific fields until I landed on Mechanical
-        Engineering. After SBCC I went to Chico State University, where I got my
-        bachelors degree in Mechanical Engineering, with a minor in
-        manufacturing. I have been working as a Mechanical Engineer for the past
-        6 years, and have decided to change career fields to focus on web
-        development and computer science.
-      </p>
-    </article>
+    <div>
+      <article id="intro">
+        <h2 className="major">Intro</h2>
+        <div id="intro-card">
+          <img src="images/me.png" alt="my_picture" className="img-main" />
+          <div id="intro-text">
+            <p>
+              Hi! My name is Brett. I am a Full Stack Developer with a
+              passionate for creation, building, and troubleshooting usefull
+              applications. I have a background in Mechanical Engineering and
+              Project Management, which is helpful when it comes to delivering
+              large and small scale projects.
+            </p>
+            <p>
+              I am currently living in Chicago. Finished a long term, intensive
+              bootcamp that focused on Front-End, Back-End, and Algorithms. By
+              the way, check out my <a href="#work">awesome work</a>.
+            </p>
+          </div>
+        </div>
+      </article>
+      <article id="about">
+        <h2 className="major">About Me</h2>
+        <p>
+          I come from a small town of Carpinteria, CA. Growing up I played
+          football, marching band, and a few random recreational activities.
+          Eagle Scout, power lifter, weight loss(er), Smash Brothers enthusiast,
+          and snacker are adjectives that I've gone by in the past.
+        </p>
+        <p>
+          After high school I spent some time at a city college (SBCC), where I
+          dabbled in most scientific fields until I landed on Mechanical
+          Engineering. After SBCC I went to Chico State University, where I got
+          my bachelors degree in Mechanical Engineering with a minor in
+          manufacturing. I have been working as a Mechanical Engineer for the
+          past 6 years, and have decided to change career fields to focus on web
+          development and computer science.
+        </p>
+      </article>
+    </div>
   );
 };
 
--- src/components/data/data.jsx
+++ src/components/data/data.jsx
@@ -21,15 +21,86 @@ export const work_details = {
     ],
   },
   react: {
-    text: "Some of my React Projects",
+    text: "Some of my Full-Stack Web Development Projects",
     projects: [
+      {
+        name: "Granted",
+        link: "https://prod-granted.herokuapp.com/",
+        desc:
+          "Team Project dedicated to matching Grant Applicants with Grant Writers for grant applications. Easy to use, easy to navigate, and easy to organize based on: if a person is applying for grants, or wanting to write for grants.",
+        roles: ["Full-Stack Developer", "Front-End", "Back-End"],
+        frameworks: [
+          "React.js",
+          "JavaScript",
+          "Redux",
+          "Jest",
+          "Material UI",
+          "Node/Express",
+        ],
+        alt: "granted site",
+        img: "",
+        github: "https://github.com/Lambda-School-Labs/grants-fe",
+        details: [
+          "Full-stack engineer in both front-end and back-end during in a 4-month long project.",
+          "Collaborated with 6 full-stack team members emphasizing: planning, team communication, pair-programming for features and troubleshooting, front-end/back-end features, debugging code, and assisting each other’s growth.",
+          "Organized pair programming sessions to increase project fluidity and team cohesiveness.",
+        ],
+      },
       {
         name: "Conway's Game of Life",
         link: "https://game-of-life-bk.vercel.app/",
+        desc:
+          "Interactive tool for simulating cellular automation (a set of cells that evolve over time based on a set of rules). Personal project for simulating Conway’s Game of Life.",
+        roles: ["Full-Stack Developer", "Front-End"],
+        frameworks: ["React.js", "Javascript", "Canvas", "Three.js"],
         alt: "game-of-life",
         img: "images/game-of-life.jpg",
+        github: "https://github.com/bwkarr77/game-of-life",
+        details: [
+          "Created a 2D game board with controls to step through the animation between generations.",
+          "Allows user to change multiple settings of the game and set their own starting generation.",
+          "Created a 3D game cube that allows the user to set pre-programmed load outs, choose different settings, set different rules to follow, and view the cube from all directions.",
+        ],
       },
-      { name: "Two", link: "none", alt: "Two", img: "" },
+      {
+        name: "Spider Graph",
+        link: "https://spider-graph-2-buildweek.netlify.app/",
+        desc:
+          "Team Project for simulating a Spider Graph/Radar Chart based on data given by the user. Allows the user to decide which “branches” of the Spider Graph are to be presented to the viewer.",
+        roles: ["Front-End Developer"],
+        frameworks: ["React.js", "Javascript", "Canvas", "Chart.js"],
+        alt: "Spider Graph",
+        img: "",
+        github: "https://github.com/spider-graph-2-build-week/front-end",
+        details: [""],
+      },
+      {
+        name: "Better Professor App",
+        link: "none",
+        desc:
+          "Remote Team App that allows students to rate their professors with ratings and reviews and assist future students with picking the best professor for their own needs.",
+        roles: ["Back-End Developer"],
+        frameworks: ["Javascript", "Node/Express", "Knex"],
+        alt: "Better Professor",
+        img: "",
+        github: "",
+        details: [
+          "Building a RESTful API for storing data, that contributed to a team of front-end developers.",
+          "Wrote organized, useful, and helpful endpoints to persist data. Provided metadata for assisting front-end developers to access, create, and manipulate data inside the API.",
+          "Delivered user authentication and authorization services with JSON web tokens.",
+        ],
+      },
+      {
+        name: "Two",
+        link: "none",
+        desc: "",
+        roles: [""],
+        frameworks: [""],
+        alt: "Two",
+        img: "",
+        github: "",
+        details: [""],
+      },
     ],
   },
 };
