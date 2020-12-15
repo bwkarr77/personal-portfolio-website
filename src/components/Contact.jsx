5cc96a2955d4cd9d94fc771a925c9641e9919c85
bwkarr77 <bwkarr77@gmail.com>
2020-12-14 22:19:45 -0600

    working on contact form, and email setup

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,22 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const Contact = () => {
+  const [contact, setContact] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (event) => {
+    setContact({
+      ...contact,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(contact);
+
+    // fetch("http://localhost:3001", {
+    //   method: "POST",
+    //   body: JSON.stringify(this.state),
+    //   header: {
+    //     Accept: "application/json",
+    //     "Content-Type": "application/json",
+    //   },
+    // })
+    //   .then((response) => response.json())
+    //   .then((response) => {
+    //     if (response.status === "success") {
+    //       alert("Message Sent.");
+    //       this.resetForm();
+    //     } else if (response.status === "fail") {
+    //       alert("Message failed to send.");
+    //     }
+    //   });
+    axios({
+      method: "POST",
+      url: "http://localhost:3001/send",
+      data: this.state,
+    }).then((response) => {
+      if (response.data.status === "success") {
+        alert("Message Sent.");
+        this.resetForm();
+      } else if (response.data.status === "fail") {
+        alert("Message failed to send.");
+      }
+    });
+  };
+
   return (
+    // console.log("contact form: ", contact),
     <article id="contact">
       <h2 className="major">Contact</h2>
-      <form method="post" action="#">
+      <form method="post" action="#" onSubmit={handleSubmit}>
         <div className="fields">
           <div className="field half">
             <label htmlFor="name">Name</label>
-            <input type="text" name="name" id="name" />
+            <input
+              type="text"
+              name="name"
+              id="name"
+              value={contact.name}
+              onChange={handleChange}
+            />
           </div>
           <div className="field half">
             <label htmlFor="email">Email</label>
-            <input type="text" name="email" id="email" />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={contact.email}
+              onChange={handleChange}
+            />
           </div>
           <div className="field">
             <label htmlFor="message">Message</label>
-            <textarea name="message" id="message" rows="4"></textarea>
+            <textarea
+              name="message"
+              id="message"
+              rows="4"
+              value={contact.message}
+              onChange={handleChange}
+            ></textarea>
           </div>
         </div>
         <ul className="actions">
