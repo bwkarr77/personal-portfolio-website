05bcfb563eb2812f8b91147f20a48e44184539d8
bwkarr77 <bwkarr77@gmail.com>
2021-01-14 20:59:46 -0600

    working on card styling for work section

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { work_details } from "./data/data.jsx";
-import "./styling.scss";
+import "./workstyling.scss";
 
 const Work = () => {
   return (
@@ -12,31 +12,51 @@ const Work = () => {
       </span> */}
       <p>{work_details.intro[0]}</p>
       <p>{work_details.intro[1]}</p>
-      <p>{work_details.css.text}</p>
-      {work_details.css.projects.map((each) => {
-        return (
-          <a href={each.link} target="_blank" key={each.name}>
-            <img src={each.img} alt={each.alt} className="css_image" />
-          </a>
-        );
-      })}
-      <p>{work_details.react.text}</p>
-      {work_details.react.projects.map((each) => {
-        return (
-          <div className="projects">
+
+      <div className="css_section">
+        <p>{work_details.css.text}</p>
+        {work_details.css.projects.map((each) => {
+          return (
             <a href={each.link} target="_blank" key={each.name}>
               <img src={each.img} alt={each.alt} className="css_image" />
             </a>
-            {each.desc}
-            Frameworks used:
-            <ul>
-              {each.frameworks.map((framework) => {
-                return <li>{framework}</li>;
-              })}
-            </ul>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
+
+      <div className="react_section">
+        <p>{work_details.react.text}</p>
+        {work_details.react.projects.map((each) => {
+          return (
+            <div className="react_projects">
+              <div className="flip_card_container">
+                <div className="flip_card_inner">
+                  <div className="flip_card_front">
+                    <a href={each.link} target="_blank" key={each.name}>
+                      <img
+                        src={each.img}
+                        alt={each.alt}
+                        className="react_image"
+                      />
+                    </a>
+                  </div>
+                  <div className="flip_card_back">
+                    <div className="react_details">
+                      {each.desc}
+                      Frameworks used:
+                      <ul>
+                        {each.frameworks.map((framework) => {
+                          return <li>{framework}</li>;
+                        })}
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </article>
   );
 };
